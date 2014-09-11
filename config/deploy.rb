636526822da0d95c1a932355d9aeedbf224159a1
Alexander <selivandex@gmail.com>
2014-09-11 23:49:46 +0600

    .

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,6 +35,7 @@ namespace :deploy do
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/plugins #{release_path}/plugins"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
