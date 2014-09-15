36d32a2786a37fb9c47302ddbc698bf2ff35890c
Marat Safin <jeizsm@gmail.com>
2014-09-15 15:54:59 +0400

    use softeam user instead redmine

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,10 @@
-root = "/home/redmine/redmine/current"
+root = "/home/softeam/redmine/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/home/redmine/redmine/shared/unicorn.sock"
+listen "/home/softeam/redmine/shared/unicorn.sock"
 # listen socket_file, :backlog => 1024
 worker_processes 2
 timeout 30
--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,10 +12,10 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/redmine/redmine/current
+APP_ROOT=/home/softeam/redmine/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=redmine
+AS_USER=softeam
 set -u
 
 OLD_PIN="$PID.oldbin"
